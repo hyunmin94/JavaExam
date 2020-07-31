7097b8e28fc0d58cc986ccd71f80ac2563ad5b49
hyunmin94 <62323327+hyunmin94@users.noreply.github.com>
2020-07-31 16:43:47 +0900

    [COMMIT] (부모클래스)Parent

---
+++ javaExamProject/src/casting/Parent.java
@@ -0,0 +1,11 @@
+package casting;
+
+public class Parent {
+	public Parent() {
+		System.out.println("부모 클래스 생성자 호출");
+	}
+	
+	public void parentMethod() {
+		System.out.println("부모 클래스 일반 메소드 호출");
+	}
+}
