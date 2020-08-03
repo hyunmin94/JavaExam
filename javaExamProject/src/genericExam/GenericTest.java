ebe9f94e0264115b2694a8a7e9013e087156fa9e
hyunmin94 <62323327+hyunmin94@users.noreply.github.com>
2020-08-03 14:37:01 +0900

    [UPDATE] 제네릭

--- javaExamProject/src/genericExam/GenericTest.java
+++ javaExamProject/src/genericExam/GenericTest.java
@@ -16,6 +16,8 @@ public class GenericTest {
 	
 	public static void main(String[] args) {
 		GenericTestClass<Integer> genericObj = new GenericTestClass<>();
+		genericObj.setObj(7);
+		System.out.println(genericObj.getObj());
 		
 		// 컴파일시 오류 발생
 		// GenericTestClass<int> genericObj = new GenericTestClass<>();
